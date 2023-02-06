4f15246a315d95f312394ee99580ba1c5ccb9437
Ayaan Irshad <54441228+ayaan-irshad@users.noreply.github.com>
2023-02-06 23:11:47 +0530

    uncmment

--- UberEatsUser/src/contexts/AuthContext.js
+++ UberEatsUser/src/contexts/AuthContext.js
@@ -1,31 +1,31 @@
-// import { createContext, useEffect, useState, useContext } from "react";
-// import { Auth, DataStore } from "aws-amplify";
-// import { User } from "../models";
+import { createContext, useEffect, useState, useContext } from "react";
+import { Auth, DataStore } from "aws-amplify";
+import { User } from "../models";
 
-// const AuthContext = createContext({});
+const AuthContext = createContext({});
 
-// const AuthContextProvider = ({ children }) => {
-// 	const [authUser, setAuthUser] = useState(null);
-// 	const [dbUser, setDbUser] = useState(null);
-// 	const sub = authUser?.attributes?.sub;
+const AuthContextProvider = ({ children }) => {
+	const [authUser, setAuthUser] = useState(null);
+	const [dbUser, setDbUser] = useState(null);
+	const sub = authUser?.attributes?.sub;
 
-// 	useEffect(() => {
-// 		Auth.currentAuthenticatedUser({ bypassCache: true }).then(setAuthUser);
-// 	}, []);
+	useEffect(() => {
+		Auth.currentAuthenticatedUser({ bypassCache: true }).then(setAuthUser);
+	}, []);
 
-// 	useEffect(() => {
-// 		DataStore.query(User, (user) => user.sub("eq", sub)).then((users) =>
-// 			setDbUser(users[0])
-// 		);
-// 	}, [sub]);
+	useEffect(() => {
+		DataStore.query(User, (user) => user.sub("eq", sub)).then((users) =>
+			setDbUser(users[0])
+		);
+	}, [sub]);
 
-// 	return (
-// 		<AuthContext.Provider value={{ authUser, dbUser, sub, setDbUser }}>
-// 			{children}
-// 		</AuthContext.Provider>
-// 	);
-// };
+	return (
+		<AuthContext.Provider value={{ authUser, dbUser, sub, setDbUser }}>
+			{children}
+		</AuthContext.Provider>
+	);
+};
 
-// export default AuthContextProvider;
+export default AuthContextProvider;
 
-// export const useAuthContext = () => useContext(AuthContext);
+export const useAuthContext = () => useContext(AuthContext);
